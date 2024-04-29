8810d39dc15f6dc5a49232c1d8496cf5c47ce46b
Fadonougbo <fadougbogautier@gmail.com>
2024-04-29 11:59:28 +0100

    update jsdoc

--- build/src/index.d.ts
+++ build/src/index.d.ts
@@ -1,15 +1,22 @@
 export declare class Wime {
+    /**
+   * Array containing hexadecimal values.
+   */
     private static hexValues;
     /**
-     * Generate random int
-     * @param Length number
-     * @returns number
+     * Generates a random number between 0 and the specified length.
+     * @param {number} Length The maximum length of the random number.
+     * @returns {number} A random number between 0 and length.
      */
     private static getRandomNumber;
+    /**
+   * Generates a random hexadecimal color.
+   * @returns {string}  A randomly generated hexadecimal color.
+   */
     private static generateHex;
     /**
-     * Add box-shadow on elements
-     * @param defaultColors
-     */
+      * Displays HTML elements with the class ".wime" by assigning them a randomly generated shadow color.
+      * @param {string[] | null} defaultColors An optional array of default colors to use, or null.
+  */
     static showElements(defaultColors?: string[] | null): void;
 }
--- build/src/index.js
+++ build/src/index.js
@@ -1,14 +1,21 @@
 // biome-ignore lint/complexity/noStaticOnlyClass: <explanation>
 export class Wime {
+    /**
+   * Array containing hexadecimal values.
+   */
     static hexValues = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "e", "f"];
     /**
-     * Generate random int
-     * @param Length number
-     * @returns number
+     * Generates a random number between 0 and the specified length.
+     * @param {number} Length The maximum length of the random number.
+     * @returns {number} A random number between 0 and length.
      */
     static getRandomNumber(Length) {
         return Math.floor(Math.random() * Length);
     }
+    /**
+   * Generates a random hexadecimal color.
+   * @returns {string}  A randomly generated hexadecimal color.
+   */
     static generateHex() {
         const n1 = Wime.hexValues[Wime.getRandomNumber(Wime.hexValues.length)];
         const n2 = Wime.hexValues[Wime.getRandomNumber(Wime.hexValues.length)];
@@ -20,9 +27,9 @@ export class Wime {
         return hex;
     }
     /**
-     * Add box-shadow on elements
-     * @param defaultColors
-     */
+      * Displays HTML elements with the class ".wime" by assigning them a randomly generated shadow color.
+      * @param {string[] | null} defaultColors An optional array of default colors to use, or null.
+  */
     static showElements(defaultColors = null) {
         const se = document.querySelectorAll(".wime");
         for (const el of se) {
