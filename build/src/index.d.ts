094219e83a247ad371b04c95179d6db06a9442a6
Fadonougbo <fadougbogautier@gmail.com>
2024-04-29 10:21:57 +0100

    change Wime class

--- build/src/index.d.ts
+++ build/src/index.d.ts
@@ -1,3 +1,6 @@
 export declare class Wine {
+    private static hexValues;
+    private static getRandomNumber;
+    private static generateHex;
     static showElement(defaultColors?: string[] | null): void;
 }
--- build/src/index.js
+++ build/src/index.js
@@ -1,14 +1,27 @@
-import { generateHex, getRandomNumber } from "./helper.js";
 // biome-ignore lint/complexity/noStaticOnlyClass: <explanation>
 export class Wine {
+    static hexValues = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "e", "f"];
+    static getRandomNumber(Length) {
+        return Math.floor(Math.random() * Length);
+    }
+    static generateHex() {
+        const n1 = Wine.hexValues[Wine.getRandomNumber(Wine.hexValues.length)];
+        const n2 = Wine.hexValues[Wine.getRandomNumber(Wine.hexValues.length)];
+        const n3 = Wine.hexValues[Wine.getRandomNumber(Wine.hexValues.length)];
+        const n4 = Wine.hexValues[Wine.getRandomNumber(Wine.hexValues.length)];
+        const n5 = Wine.hexValues[Wine.getRandomNumber(Wine.hexValues.length)];
+        const n6 = Wine.hexValues[Wine.getRandomNumber(Wine.hexValues.length)];
+        const hex = `#${n1}${n2}${n3}${n4}${n5}${n6}`;
+        return hex;
+    }
     static showElement(defaultColors = null) {
         const se = document.querySelectorAll(".wime");
         for (const el of se) {
-            let color = generateHex();
+            let color = Wine.generateHex();
             if (Array.isArray(defaultColors) && defaultColors.length > 0) {
-                color = defaultColors[getRandomNumber(defaultColors.length)];
+                color = defaultColors[Wine.getRandomNumber(defaultColors.length)];
             }
-            el.style.setProperty("box-shadow", `2px 2px  ${color} inset,-2px -2px ${color} inset`);
+            el.style.setProperty("box-shadow", `0.2rem 0.2rem  ${color} inset,-0.2rem -0.2rem ${color} inset`);
         }
     }
 }
